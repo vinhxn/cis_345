acea008727c9f5aed4da01fdadb820a881c789d9
nxuanvinh <nxuanvinh@hotmail.com>
2024-10-02 08:37:25 +0700

    Update messageprint.c

--- Project2/p2/messageprint.c
+++ Project2/p2/messageprint.c
@@ -1,21 +1,20 @@
 /*
  * messageprint.c
- *
+ * Nhat Nguyen
  */
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-//Initialize Global Variables and Include Necessary Headers
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t CV = PTHREAD_COND_INITIALIZER;
-int number_prime_finished = 0;
-int total_threads;
-bool *is_prime;
+int number_prime_finished = 0; // number of prime printed
+int total_threads; // total number of threads
+int total_prime_threads = 0; //count number of prime threads
 
-//Function to Check Prime Numbers
-bool check_prime(int num) {
+// Function to check if the number of prime
+bool isPrime(int num) {
     if (num <= 1) return false;
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return false;
@@ -23,49 +22,61 @@ bool check_prime(int num) {
     return true;
 }
 
+
 //Thread function
 void* thread_function(void* arg) {
-    int thread_id = *(int*)arg;
+    int thread_id = *((int*)arg);
 
     pthread_mutex_lock(&lock);
-    if (is_prime[thread_id]) {
-        printf("Prime Thread %d: Printing message\n", thread_id);
-        number_prime_finished++;
-        if (number_prime_finished == total_threads) {
-            pthread_cond_broadcast(&CV);
-        }
-    } else {
-        while (number_prime_finished < total_threads) {
+    if (!isPrime(thread_id)) {
+        //if this thread_id is not prime, wait until all prime threads have printed
+        while (number_prime_finished < total_prime_threads) {
             pthread_cond_wait(&CV, &lock);
         }
-        printf("Composite Thread %d: Printing message\n", thread_id);
     }
     pthread_mutex_unlock(&lock);
+    //print thread
+    printf("Thread %d: Hello, I am %s.\n", thread_id, isPrime(thread_id) ? "prime" : "composite");
+
+    pthread_mutex_lock(&lock);
+
+    //count prime threads
+    if (isPrime(thread_id)) {
+        number_prime_finished++;
+        //if all prime threads are finished, wakes up all threads that are sleeping
+        if (number_prime_finished == total_prime_threads) {
+            pthread_cond_broadcast(&CV);
+        }
+    }
+    pthread_mutex_unlock(&lock);
+
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <number_of_threads>\n", argv[0]);
         return 1;
     }
 
     total_threads = atoi(argv[1]);
-    if (total_threads < 20) {
-        fprintf(stderr, "Number of threads should be at least 20.\n");
+    if (total_threads < 1) {
+        fprintf(stderr, "Number of threads must be at least 1.\n");
         return 1;
     }
 
-    pthread_t threads[total_threads];
-    int thread_ids[total_threads];
-    is_prime = malloc(total_threads * sizeof(bool));
-
-    for (int i = 0; i < total_threads; i++) {
-        thread_ids[i] = i;
-        is_prime[i] = check_prime(i);
+    // calculate number of prime threads
+    for(int i = 0; i < total_threads; i++) {
+        if(isPrime(i))
+            total_prime_threads++;
     }
 
+    // create threads
+    pthread_t threads[total_threads];
+    int thread_ids[total_threads];
+
     for (int i = 0; i < total_threads; i++) {
+        thread_ids[i] = i + 1;
         pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]);
     }
 
@@ -73,6 +84,6 @@ int main(int argc, char *argv[]) {
         pthread_join(threads[i], NULL);
     }
 
-    free(is_prime);
     return 0;
 }
+
